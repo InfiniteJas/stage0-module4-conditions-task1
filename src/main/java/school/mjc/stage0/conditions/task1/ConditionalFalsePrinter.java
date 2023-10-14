2a5f25765dec4c8a48d93fe036384f1d54b3e14c
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-14 22:38:24 +0600

    Module 4.1 task 1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean condition = false;
+
+        if (condition) {
+            // This block won't be executed because the condition is false.
+            System.out.println("true");
+        } else {
+            // This block will be executed because the else part is triggered when the condition is false.
+            System.out.println("false");
+        }
     }
 }
